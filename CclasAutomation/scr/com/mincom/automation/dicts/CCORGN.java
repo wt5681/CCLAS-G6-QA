edb5ac7d5d758b34b1f8065654003354aa6f90f6
Vuong Huynh <hlvuong@gmail.com>
2012-01-06 14:47:23 +1000

    Remove old file and re-arrange project

--- CclasAutomation/scr/com/mincom/automation/dicts/CCORGN.java
+++
@@ -1,9 +0,0 @@
-package com.mincom.automation.dicts;
-
-public class CCORGN {
-	/***********Define controls on CCORGN Search screen**************************/
-	public static String ccorgnSearch_searchMethod="searchMethod";
-	public static String ccorgnSearch_location="location";
-	public static String ccorgnSearch_resultsGrid="resultsGrid";
-	/***********Define controls on CCORGN ..............**************************/
-}
